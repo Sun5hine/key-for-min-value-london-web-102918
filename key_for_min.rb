d82fe6160852bd05d7e45510aa403fea5c73b421
sun5hine <af5kh@yahoo.com>
2018-10-23 18:06:31 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,6 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-name_hash {|x, y| puts x if 
-end
+ k = name_hash.map {|x, y| y }.min
+name_hash.map {|x, y| x if y == k  }.compact
+end 
